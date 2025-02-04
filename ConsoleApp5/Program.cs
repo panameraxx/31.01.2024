6558dd7c139ce03ce5ef368e4da73121c613713f
panameraxx <90213411+panameraxx@users.noreply.github.com>
2025-02-04 21:45:38 +0200

    Add files via upload

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Time t1 = new Time();
+            t1.h = 1;
+            t1.m = 120;
+            t1.s = 500;
+            t1.ShowInfo();
+
+            Time t2 = new Time(15, 25, 200);
+            Console.WriteLine($"{t2.Tostring()}");
+
+            Console.WriteLine("Введите часы: ");
+            int h1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите минуты: ");
+            int m1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите секунды: ");
+            int s1 = int.Parse(Console.ReadLine());
+
+            Time t3 = new Time(h1, m1, s1);
+            Console.WriteLine($"{t3.Tostring()}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ ConsoleApp5/Time.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Time
+    {
+        public int h;
+        public int m;
+        public int s;
+
+        public Time() { }
+
+        public Time(int h)
+        {
+            this.h = h;
+        }
+
+        public Time(int h, int m) : this(h)
+        {
+            this.m = m;
+        }
+
+        public Time(int h, int m, int s) : this(h, m)
+        {
+            this.s = s;
+        }
+
+        public int Timeins()
+        {
+            int rez = h * 60;
+            int rez2 = rez + m;
+            return rez2 * 60 + s;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Часы: {h}\t Минуты: {m}\t Секунды: {s}\t Результат: {Timeins()} секунд");
+        }
+
+        public string Tostring()
+        {
+            return $"Часы: {h}\t Минуты: {m}\t Секунды: {s}\t Результат: {Timeins()} секунд";
+        }
+    }
+
+}
