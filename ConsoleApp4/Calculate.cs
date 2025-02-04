0c99c336112fae410f26b9f1a9cb4bd514f5a176
panameraxx <90213411+panameraxx@users.noreply.github.com>
2025-02-04 21:40:19 +0200

    Add files via upload

---
+++ ConsoleApp4/Calculate.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Calculate
+    {
+        public double numA;
+        public double numB;
+        public double rez;
+
+        public Calculate() { }
+
+        public Calculate(double numA)
+        {
+            this.numA = numA;
+        }
+
+        public Calculate(double numA, double numB) : this(numA)
+        {
+            this.numB = numB;
+        }
+
+        public double CalcSQRT()
+        {
+            return Math.Sqrt(numA * numB);
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Первое число: {numA}\t Второе число: {numB}\t Результат: {CalcSQRT():f2}");
+        }
+
+        public string Tostring()
+        {
+            return $"Первое число: {numA}\t Второе число: {numB}\t Результат: {CalcSQRT():f2}";
+        }
+    }
+
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculate c1 = new Calculate();
+            c1.numA = 10;
+            c1.numB = 2;
+            c1.ShowInfo();
+
+            Calculate c2 = new Calculate(15, 2);
+            Console.WriteLine($"{c2.Tostring()}");
+
+            Console.WriteLine("Введите первое число: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число: ");
+            double b = double.Parse(Console.ReadLine());
+
+            Calculate c3 = new Calculate(a, b);
+            Console.WriteLine($"{c3.Tostring()}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
